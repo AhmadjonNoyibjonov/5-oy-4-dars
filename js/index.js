8739fe929a214d8162f5245a548dc28cc0e4b3d2
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-06-23 04:03:47 +0500

    project

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const hiElement = document.getElementById("hi")
+// const classname = document.getElementsByClassName("user");
+// const tagname = document.getElementsByTagName("p");
+
+// let users = document.querySelector('p');
+// users.setAttrubute('user')
+
+// p.innerHTML = "Ahrorbek"
+
+// const button1 = document.querySelector("header button");
+
